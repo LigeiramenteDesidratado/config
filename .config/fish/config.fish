2554acc7f70b8a257d5f1128736a64285e2091c8
Vinicius de Oliveira <vinicius.vncy@gmail.com>
2019-05-04 08:13:49 -0300

    up

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,12 +5,12 @@ set -gx GOBIN $GOPATH/bin
 
 set -gx EDITOR /bin/nvim
 set -gx TERMINAL /usr/local/bin/st
-set -gx BROWSER /usr/bin/brave
+set -gx BROWSER /usr/bin/brave-dev
 set -gx READER /usr/bin/zathura
 set -gx COCOS_X_ROOT /opt/cocos2d-x
 set -gx COCOS_CONSOLE_ROOT $COCOS_X_ROOT/tools/cocos2d-console/bin
 set -gx COCOS_TEMPLATES_ROOT $COCOS_X_ROOT/templates
-set PATH $GOPATH $GOBIN $COCOS_X_ROOT $COCOS_CONSOLE_ROOT $COCOS_TEMPLATES_ROOT $PATH /home/machine/.scripts/ $HOME/.cargo/bin
+set PATH $GOPATH $GOBIN $COCOS_X_ROOT $COCOS_CONSOLE_ROOT $COCOS_TEMPLATES_ROOT $PATH /home/machine/.scripts/ $HOME/.cargo/bin $HOME/.scripts/i3blocks/
 
 source ~/.aliases
 
