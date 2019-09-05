c907d1728cb99b646c46bebdfa406c4367f4b2cd
Vinicius de Oliveira <vinicius.vncy@gmail.com>
2019-09-05 13:45:35 -0300

    up

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,17 +1,16 @@
 set fish_emoj_width 2
 set fish_ambiguous_width 1
-set -gx GOPATH $HOME/dev/golang/goworkspace
+set -gx GOPATH $HOME/dev/golang/
 set -gx GOBIN $GOPATH/bin
 
 set -gx EDITOR /bin/nvim
 set -gx TERMINAL /usr/local/bin/st
 set -gx BROWSER /usr/bin/brave-dev
 set -gx READER /usr/bin/zathura
-# set -gx BRAVE_FLAGS --kiosk
-set PATH $GOPATH $GOBIN $COCOS_X_ROOT $COCOS_CONSOLE_ROOT $COCOS_TEMPLATES_ROOT $PATH $HOME/.scripts/ $HOME/.local/bin/ $HOME/.cargo/bin $HOME/.scripts/i3blocks/ (yarn global bin)
+
+set PATH $GOPATH $GOBIN $PATH $HOME/.scripts/ $HOME/.local/bin/ $HOME/.cargo/bin $HOME/.scripts/i3blocks/ (yarn global bin)
 
 source ~/.aliases
-remaps&
 
 # Start X at login
 # if status is-login
