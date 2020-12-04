f8e95868b1715936ae1740b4454a2e57579e92b5
vinicius <viniciusxyz@protonmail.com>
2020-12-04 20:27:44 -0300

    [nvim] support for TeX (needs improvement)

---
+++ .config/nvim/lua/l/TeX/init.lua
@@ -0,0 +1,43 @@
+-- @module l.TeX
+
+local layer = {}
+
+local plug = require("c.plug")
+
+--- Returns plugins required for this layer
+function layer.register_plugins()
+  plug.add_plugin("lervag/vimtex")
+end
+
+--- Configures vim and plugins for this layer
+function layer.init_config()
+  local lsp = require("l.lsp")
+  local nvim_lsp = require("lspconfig")
+
+  -- vim.g.completion_chain_complete_list = {
+  --   ["tex"] = {
+  --     {["complete_items"] = {"lsp", "vimtex"}},
+  --   }
+  -- }
+
+  -- require'completion'.addCompletionSource('vimtex', layer.complete_item)
+
+  vim.g.vimtex_view_method = 'zathura'
+
+	vim.g.tex_conceal = ''
+  lsp.register_server(nvim_lsp.texlab)
+end
+
+-- function layer.get_completion_items(prefix)
+
+--   local item = vim.api.nvim_call_function('vimtex#complete#omnifunc',{0, prefix})
+
+--   return item
+-- end
+
+-- layer.complete_item = {
+--   item = layer.get_completion_items
+-- }
+
+
+return layer
