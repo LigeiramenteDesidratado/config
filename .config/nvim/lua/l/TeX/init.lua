68d6c128212e26d28060be397969cff15921d93e
vinicius <viniciusxyz@protonmail.com>
2020-12-11 19:16:06 -0300

    [nvim] Vimtex - Configure conceal and use zathura as doc viewer

--- .config/nvim/lua/l/TeX/init.lua
+++ .config/nvim/lua/l/TeX/init.lua
@@ -23,8 +23,15 @@ function layer.init_config()
   -- require'completion'.addCompletionSource('vimtex', layer.complete_item)
 
   vim.g.vimtex_view_method = 'zathura'
+  vim.g.tex_flavor = 'latex'
+  vim.g.vimtex_quickfix_mode = 0
 
-	vim.g.tex_conceal = ''
+  -- This is a feature where LaTeX code is replaced or made invisible when your cursor is not on that line
+  -- I don't know why using meta-accessors doesn't work
+  -- vim.o.conceallevel=2
+  vim.cmd("set conceallevel=2")
+  vim.g.tex_conceal = 'abdmg'
+
   lsp.register_server(nvim_lsp.texlab)
 end
 
